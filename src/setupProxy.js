c57cef1b39cf6d9f951fca8bd85f80f7bb8c6450
chalinuna <mr.unil.c@gmail.com>
2023-01-15 07:24:10 +0900

    proxy, redirects, package 수정함

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "Movie-env.eba-e2vabrjf.ap-northeast-1.elasticbeanstalk.com",
+      target: "http://www.movieapp.shop/",
       changeOrigin: true,
     })
   );
